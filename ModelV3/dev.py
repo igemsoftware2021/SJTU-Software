e53235bc795583bbec77d69009b7e9707f1fe4b0
ComDec <1811392681@qq.com>
2021-10-16 13:54:01 +0800

    Update dev.py

--- ModelV3/dev.py
+++ ModelV3/dev.py
@@ -1,58 +1,192 @@
 # import essential package
+from matplotlib import pyplot as plt
+from sklearn.metrics import precision_score
 from torch.utils.data import DataLoader
-from Getdataset import IntelDNADataset
-
+from utils.Getdataset import IntelDNADataset
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.auto import tqdm
 # utils
-from utils.aligning import bp_align, de_align
-from utils.embedding import OneHotEmbedding
+from utils.aligning import *
+from utils.embedding import *
+from utils.label_embedding import *
 from utils.mask import *
-import matplotlib.pyplot as plt
-
+# loss function
+from utils._loss import *
 # model
 from model.attention_model import *
-from sklearn.metrics import f1_score, precision_score
+from utils.calculate import model_structure
 import warnings
 
 warnings.filterwarnings("ignore")
 
 # Hyper-Parameters
 batch_size = 1
+wv_size = 16
 lr = 0.0005
-global_length = 500
-n_epoch = 1
+global_length = 100
+n_epoch = 2000
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+Experiment_name = 'ModelV2'
+
+# Create tensorboard
+writer = SummaryWriter(Experiment_name)
 
 # Definition of Embedding Function
-Embedder = OneHotEmbedding(4)
-LabelEmbedder = OneHotEmbedding(2)
-position = PositionalEncoding(8, global_length)
+LabelEmbedder = LabelOneHotEmbedding(3)
+position = PositionalEncoding(wv_size, global_length)
 
 # Definition of model
-model_bs = 128
-AttentionLayer = MODELI(8, 1, 1, 1, model_bs, 0.25, global_length)
+torch.cuda.empty_cache()
+model_bs = 256
+AttentionLayer = MODELO(wv_size, 4, 1, 2, model_bs, 0.25, global_length)
 AttentionLayer = AttentionLayer.to(device)
+criterion_bce_weighted = torch.nn.BCEWithLogitsLoss()
+criterion = f1_loss
 # Load model with dict
-param_load_model = torch.load("./Model_I10.9358125329017639.pth", map_location='cuda')
+param_load_model = torch.load("./ModelV2_Stage0.pth", map_location='cuda')
 AttentionLayer.load_state_dict(param_load_model)
 
+
+# calculate parameters
+model_structure(AttentionLayer)
+
+# Optimizer & Loss Function
+opti = torch.optim.Adam(AttentionLayer.parameters(), lr=lr)
 # Create dataset
-devSet = IntelDNADataset('./full_data/bpRNA_dataset', './full_data/adj_dataset_full',
-                         './full_data/node2vec_dataset_full', global_length, 'dev')
-dev_dataloader = DataLoader(devSet, batch_size=batch_size, shuffle=True, drop_last=True)
+devSet = IntelDNADataset('./dataset/bpRNA100', './dataset/bpRNA100_ADJ', global_length, wv_size, 'test')
+dev_dataloader = DataLoader(devSet, batch_size=batch_size, shuffle=False, drop_last=True)
 
-# Step to record
-dev_step = 0
+# minimum  Accuracy
+min_acc = torch.tensor(0.85).to(device)
 
-# Create base dict
-src_vocab = {'A': 0, 'T': 1, 'C': 2, 'G': 3, 'U': 1, '#': 4, 'R': 4, 'Y': 4, 'M': 4, 'K': 4, 'S': 4, 'W': 4, 'B': 4,
-             'V': 4, 'D': 4, 'N': 4, 'I': 4}
+# Create base dict notation "4" represent for padding mask
+src_vocab = {'A': 0, 'T': 1, 'C': 2, 'G': 3, 'U': 1, '#': 4}
 
 total_acc = []
-min_acc = 99
 total_f1 = []
 total_presion = []
 total_rog = []
 cohen_score = []
+
+# Main Epoch
+for epoch in range(n_epoch):
+
+    # Definition of Model Name
+    name = Experiment_name
+
+    print("Epoch{}".format(epoch))
+
+    # Training Mode
+    AttentionLayer.train()
+
+    for data in tqdm(dev_dataloader):
+
+        # Load data
+        # seq, encode_bp, label, adj, Codes, lens = data
+
+        seq, wv_align_bp, label, adj, lens = data
+
+        # wv_align_bp = encodecp_align(encode_bp, batch_size, wv_size, global_length)
+        align_label = target_align(label, batch_size, global_length)
+
+        # Data encode for src_padding_mask
+        enc_input = []
+        for batch in range(batch_size):
+            enc_input.append([])
+            for i in range(len(seq[batch])):
+                enc_input[batch].append([src_vocab[n] for n in seq[batch][i].split()])
+        enc_input = torch.tensor(enc_input)
+        enc_input = enc_input.to(device)
+        enc_input = enc_input.view(batch_size, -1)
+
+        # Src padding mask
+        src_mask = padding_mask(enc_input, global_length)
+
+        # One hot Encoding
+        encode_bp = [0 for x in range(batch_size)]
+        encode_label = [0 for x in range(batch_size)]
+        for i in range(batch_size):
+            encode_label[i] = LabelEmbedder.encode(align_label[i])
+
+        encode_bp = torch.tensor(wv_align_bp)
+        encode_bp = encode_bp.permute(1, 0, 2)
+
+        encode_label = torch.tensor(encode_label)
+        encode_label = encode_label.permute(0, 2, 1)
+
+        encode_label_index = torch.argmax(encode_label, dim=2)
+        encode_label_index = encode_label_index.to(device)
+
+        encode_bp = encode_bp.to(device)
+        encode_label = encode_label.to(device)
+        adj = adj.to(device)
+
+        bp_input = position(encode_bp)
+        bp_input = bp_input.permute(1, 0, 2)
+
+        ## encode_label = encode_label.long()  # That's important, or IDE will deliver error
+
+        # contact_masks = torch.Tensor(contact_map_masks(lens, global_length)).to(device)
+        #
+        # contacts = [0 for x in range(batch_size)]
+        #
+        # for i in range(batch_size):
+        #     contacts[i] = dash2matrix(align_label[i], global_length)
+        #
+        # contacts = torch.tensor([item.cpu().detach().numpy() for item in contacts])
+        #
+        # contacts_batch = contacts.float().to(device)
+
+        bp_input = bp_input.repeat(1, model_bs, 1)
+
+        src_mask = src_mask.repeat(model_bs, 1)
+
+        adj = adj.repeat(model_bs, 1, 1)
+
+        # Prediction
+        with torch.no_grad():
+
+            out = AttentionLayer(bp_input, adj, src_mask)
+
+        index_out = torch.argmax(seq[0], dim=1)
+
+        index_out = index_out.to(device)
+
+        # target_names = ['class 0', 'class 1', 'class 2']
+        #
+        # print(classification_report(encode_label_index[0].detach().cpu().tolist(), index_out[0].detach().cpu().tolist(), target_names=target_names))
+
+        f1 = f1_score(encode_label_index[0].detach().cpu().tolist(), index_out.detach().cpu().tolist(),
+                      average='micro')
+        #
+        precison = precision_score(encode_label_index[0].detach().cpu().tolist(),
+                                   index_out.detach().cpu().tolist(), average='micro')
+
+        # rog = roc_auc_score(encode_label_index[0].detach().cpu().tolist(), index_out[0].detach().cpu().tolist(), average='weighted', multi_class='ovr')
+
+        acc = (index_out == encode_label_index).float().mean()
+
+        acc = acc.detach().cpu().tolist()
+
+        index_out = index_out.detach().cpu().tolist()
+
+        # cohen_score.append(
+        #     cohen_kappa_score(encode_label_index[0].detach().cpu().tolist(), index_out[0].detach().cpu().tolist()))
+
+        if acc < min_acc:
+            min_acc = acc
+
+        total_acc.append(acc)
+        total_f1.append(f1)
+        total_presion.append(precison)
+        # total_rog.append(rog)
+
+        print('Acc:{}'.format(acc))
+        print('Label {}'.format(label[0]))
+        print('F1:{}'.format(f1))
+        print('Presion:{}'.format(precison))
+
+
 
 
 def list2average(list):
@@ -76,129 +210,6 @@ def class2binnota(x):
         return '.'
     if x == 1:
         return '@'
-
-
-# Main Epoch
-for epoch in range(n_epoch):
-
-    # Definition of Model Name
-
-    # De Mode
-    AttentionLayer.eval()
-
-    for data in dev_dataloader:
-
-        # Accuracy
-        dev_acc = 0
-
-        # Load data
-        bp, label, adj, vec, name = data
-
-        bp = [x.upper() for x in bp]
-
-        length = len(bp)
-
-        # Data aligning
-        align_bp, align_label = bp_align(bp, label, batch_size, max_len=global_length)
-
-        # Create Adj_matrix
-        for i in range(batch_size):
-            adj[i] = buildAdjandIdxmap(adj[i], global_length)
-
-        adj = adj.detach().cpu().numpy()
-        adj = torch.from_numpy(adj)
-        adj = adj.to(device)
-
-        # Data encode for src_padding_mask
-        enc_input = []
-        for batch in range(batch_size):
-            enc_input.append([])
-            for i in range(len(align_bp[batch])):
-                enc_input[batch].append([src_vocab[n] for n in align_bp[batch][i].split()])
-
-        enc_input = torch.tensor(enc_input)
-        enc_input = enc_input.to(device)
-        enc_input = enc_input.view(batch_size, -1)
-
-        # Src padding mask
-        src_mask = padding_mask(enc_input, global_length)
-
-        # One hot Encoding
-        encode_bp = [0 for x in range(batch_size)]
-        encode_label = [0 for x in range(batch_size)]
-        for i in range(batch_size):
-            encode_bp[i] = Embedder.encode(align_bp[i])
-            encode_label[i] = LabelEmbedder.encode(label[i])
-        encode_bp = torch.tensor(encode_bp)
-        encode_bp = encode_bp.permute(2, 0, 1)
-
-        encode_label = torch.tensor(encode_label)
-        encode_label = encode_label.permute(0, 2, 1)
-
-        encode_label_index = torch.argmax(encode_label, dim=2)
-        encode_label_index = encode_label_index.to(device)
-
-        encode_bp = encode_bp.to(device)
-        encode_label = encode_label.to(device)
-        encode_label = encode_label.long()  # That's important, or IDE will deliver error
-
-        vec = vec.permute(1, 0, 2).to(device)
-
-        # L, B, d
-        pos_input = position(torch.cat((encode_bp, vec), dim=2)).float()
-
-        pos_input = pos_input.repeat(1, model_bs, 1)
-
-        src_mask = src_mask.repeat(model_bs, 1)
-
-        adj = adj.repeat(model_bs, 1, 1)
-
-        with torch.no_grad():
-            # Prediction
-            out = AttentionLayer(pos_input, adj, src_mask)
-
-            seq, _ = de_align(out, encode_label, batch_size, encode_label.shape[1])
-
-            index_out = torch.argmax(seq[0], dim=1)
-
-            index_out = index_out.to(device)
-
-            # target_names = ['class 0', 'class 1', 'class 2']
-            #
-            # print(classification_report(encode_label_index[0].detach().cpu().tolist(), index_out[0].detach().cpu().tolist(), target_names=target_names))
-
-            f1 = f1_score(encode_label_index[0].detach().cpu().tolist(), index_out.detach().cpu().tolist(),
-                          average='micro')
-            #
-            precison = precision_score(encode_label_index[0].detach().cpu().tolist(),
-                                        index_out.detach().cpu().tolist(), average='micro')
-
-            # rog = roc_auc_score(encode_label_index[0].detach().cpu().tolist(), index_out[0].detach().cpu().tolist(), average='weighted', multi_class='ovr')
-
-            acc = (index_out == encode_label_index).float().mean()
-
-            acc = acc.detach().cpu().tolist()
-
-            index_out = index_out.detach().cpu().tolist()
-
-            pred = [class2binnota(x) for x in index_out]
-
-            # cohen_score.append(
-            #     cohen_kappa_score(encode_label_index[0].detach().cpu().tolist(), index_out[0].detach().cpu().tolist()))
-
-            if acc < min_acc:
-                min_acc = acc
-
-            total_acc.append(acc)
-            total_f1.append(f1)
-            total_presion.append(precison)
-            # total_rog.append(rog)
-
-            print('Acc:{}'.format(acc))
-            print('Label {}'.format(label[0]))
-            print('Pred ', ''.join(pred))
-            print('F1:{}'.format(f1))
-            print('Presion:{}'.format(precison))
 
 
     print('Min_acc: {}'.format(min_acc))
